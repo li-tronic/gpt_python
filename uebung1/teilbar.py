f4b06d404484e55447e1e6346548ac6f521490c1
Marius Bertram <marius.bertram@stud.uni-due.de>
2018-04-27 06:31:38 +0200

    Update teilbar.py

--- uebung1/teilbar.py
+++ uebung1/teilbar.py
@@ -2,7 +2,7 @@
 zahl1 = int(input("Zahl 1: "))
 zahl2 = int(input("Zahl 2: "))
 
-if (zahl1 % zahl2 == 0) ^ (zahl2 % zahl1 == 0 ):
+if (zahl1 % zahl2 == 0) or (zahl2 % zahl1 == 0 ):
     print(True)
 else:
     print(False)
